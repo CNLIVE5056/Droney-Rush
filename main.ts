a61adb980b22dca0a7d28a459be364ac6606efd1
CNLIVE5056 <161984537+CNLIVE5056@users.noreply.github.com>
2025-01-17 00:11:41 -0600

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -149,9 +149,9 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
                 Rocket = sprites.createProjectileFromSprite(SelectSprite(1), blaster, 100, 0)
             } else if (sprite0 == 1) {
                 if (life <= 2.8) {
-                    story.spriteSayText(Droney, "Healing...Life:" + convertToText(Math.round((life + 0.5) * 10) / 10))
-                    life += 0.5
-                    life = Math.round(life * 10) / 10
+                    story.spriteSayText(Droney, "Healing")
+                    life += 1
+                    life = Math.round(life)
                     if (life < 2) {
                         lives.setImage(assets.image`myImage2`)
                     } else if (life < 3) {
